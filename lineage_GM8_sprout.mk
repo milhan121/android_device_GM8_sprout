e3173e9fe8d4f98df04ee12eca4748badc655074
Muharrem İlhan <130662616+milhan121@users.noreply.github.com>
2024-11-04 15:04:41 +0300

    Update lineage_GM8_sprout.mk
    change product flags

--- lineage_GM8_sprout.mk
+++ lineage_GM8_sprout.mk
@@ -8,17 +8,17 @@
 
 $(call inherit-product, device/gm/GM8_sprout/device.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common.mk)
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_GM8_sprout
 PRODUCT_DEVICE := GM8_sprout
-PRODUCT_BRAND := GM
+PRODUCT_BRAND := General Mobile
 PRODUCT_MODEL := GM 8
 PRODUCT_MANUFACTURER := General Mobile
 
